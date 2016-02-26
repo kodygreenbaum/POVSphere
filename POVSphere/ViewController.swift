11f89b5973ec2d56ea73155ba696361cc8e1c3d7
Kody <kodygreenbaum@gmail.com>
2016-02-26 14:01:16 -0600

    Initial commit

---
+++ POVSphere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POVSphere
+//
+//  Created by Kody Greenbaum on 2/26/16.
+//  Copyright © 2016 Kode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
