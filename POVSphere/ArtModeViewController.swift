ee84ff68f8e5b2e43968474a21821bb0dc0154ae
Kody <kodygreenbaum@gmail.com>
2016-04-28 12:25:56 -0500

    Art mode working well. Still Slow updating. Cleaned up project to delete unnecessary art mode stuff.

--- POVSphere/ArtModeViewController.swift
+++ POVSphere/ArtModeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import ColorSlider
 import CoreBluetooth
 
-class ArtModeViewController: UIViewController, UITextFieldDelegate {
+class ArtModeViewController: UIViewController {
     
     let colorSlider = ColorSlider()
     var lastPoint = CGPoint.zero
@@ -21,7 +21,6 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
     var brushWidth: CGFloat = 8.0
     var opacity: CGFloat = 1.0
     var swiped = false
-    var textfieldEditing = false
     
     private let xpix : CGFloat = 150.0
     private let ypix : CGFloat = 66.0
@@ -37,26 +36,13 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
     private var buffStart = 0
     private var buffEnd = 0
     private var writing = false
-    
-    
+
     
     @IBOutlet var containerView: UIView!
-    @IBOutlet weak var saveStaticButton: UIButton!
-    @IBOutlet weak var newModeNameLabel: UILabel!
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var saveButton: UIButton!
-    @IBOutlet weak var cancelButton: UIButton!
-    @IBOutlet weak var undoButton: UIButton!
     @IBOutlet weak var sliderContainerView: UIView!
     @IBOutlet weak var selectedColorview: UIView!
     @IBOutlet weak var mainImageView: UIImageView!
     @IBOutlet weak var tempImageView: UIImageView!
-    @IBOutlet weak var redLabel: UILabel!
-    @IBOutlet weak var greenLabel: UILabel!
-    @IBOutlet weak var blueLabel: UILabel!
-    @IBOutlet weak var charValueLabel: UILabel!
-    
-    @IBOutlet weak var saveStaticVerticalConstraint: NSLayoutConstraint!
     
     @IBAction func clearButtonPressed(sender: AnyObject) {
         var x = 1
@@ -71,91 +57,6 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func doneButtonPressed(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    @IBAction func undoButtonPressed(sender: AnyObject) {
-        tempImageView.image = nil
-        undoButton.hidden = true
-    }
-    
-    @IBAction func saveAsStaticModeButtonPressed(sender: AnyObject) {
-        self.textField.text = ""
-        self.textField.hidden = false
-        self.newModeNameLabel.hidden = false
-        self.saveButton.hidden = false
-        self.cancelButton.hidden = false
-        self.saveStaticButton.hidden = true
-    }
-    
-    @IBAction func saveButtonPressed(sender: AnyObject) {
-        
-        if(textField.text == "" || textField.text == " ") {
-            // Present Alert View
-            let alert =  UIAlertController(title: NSLocalizedString("No Name", comment: "No Name"), message:NSLocalizedString("Please name your new mode.", comment: "Not Saved") , preferredStyle: .Alert)
-            let okAction =  UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
-                self.textField.becomeFirstResponder()
-            })
-            alert.addAction(okAction)
-            self.presentViewController(alert, animated: true, completion: nil)
-            
-        } else {
-            let newMode : Mode = Mode(name: self.textField.text!.lowercaseString, modeByte: 7)
-            let nav : UINavigationController = self.presentingViewController as! UINavigationController
-            let tab : UITabBarController = nav.viewControllers[0] as! UITabBarController
-            
-            let pres : StaticModeSelectViewController = tab.viewControllers![0] as! StaticModeSelectViewController
-            pres.staticModes.append(newMode)
-            self.saveButton.hidden = true
-            self.cancelButton.hidden = true
-            self.textField.text = ""
-            self.textField.hidden = true
-            self.newModeNameLabel.hidden = true
-            saveStaticButton.hidden = false
-            textField.resignFirstResponder()
-            self.saveStaticVerticalConstraint.constant = 20
-            UIView.animateWithDuration(NSTimeInterval(0.25), animations: {
-                self.containerView.layoutIfNeeded()
-            })
-            
-            // Present Alert View
-            let alert =  UIAlertController(title: NSLocalizedString("Great Success!", comment: "Saved New Mode"), message:NSLocalizedString(newMode.name + " was saved.", comment: "Mode Saved") , preferredStyle: .Alert)
-            
-            let okAction =  UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
-            })
-            alert.addAction(okAction)
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
-    }
-    
-    @IBAction func cancelButtonPressed(sender: AnyObject) {
-        self.saveButton.hidden = true
-        self.cancelButton.hidden = true
-        self.textField.text = ""
-        self.textField.hidden = true
-        self.newModeNameLabel.hidden = true
-        saveStaticButton.hidden = false
-        textField.resignFirstResponder()
-        self.saveStaticVerticalConstraint.constant = 20
-        UIView.animateWithDuration(NSTimeInterval(0.25), animations: {
-            self.containerView.layoutIfNeeded()
-        })
-
-    }
-    
-  
-    @IBAction func nabDatDataPressed(sender: AnyObject) {
-        var globeArray = [[UInt32]](count: Int(xpix), repeatedValue: [UInt32](count: Int(ypix), repeatedValue: 0))
-        globeArray = mapPixelsToGlobe(Int(xpix), yPixels: Int(ypix))
-        // Send Array as bitmap over bluetooth
-    }
-    
-    @IBAction func sendColorButtonPressed(sender: AnyObject) {
-        var color: UInt32 = 0
-        color = hexValue(colorSlider.color)
-        
-        if let data: NSData? = NSData(bytes: &color, length: 4) {
-            periph.writeValue(data!, forCharacteristic: writeChar, type: CBCharacteristicWriteType.WithResponse)
-        }
     }
     
     override func viewDidLoad() {
@@ -185,28 +86,6 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
         
     }
 
-    
-    // MARK: TextField Delegate Methods
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        self.saveStaticVerticalConstraint.constant = 20
-        UIView.animateWithDuration(NSTimeInterval(0.25), animations: {
-            self.containerView.layoutIfNeeded()
-        })
-        textfieldEditing = false
-        return true
-    }
-    
-    
-    func textFieldDidBeginEditing(textField: UITextField) {
-        self.saveStaticVerticalConstraint.constant = 212
-        UIView.animateWithDuration(NSTimeInterval(0.25), animations: {
-            self.containerView.layoutIfNeeded()
-        })
-        textfieldEditing = true
-    }
-
-    
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
         return UIInterfaceOrientationMask.LandscapeLeft
     }
@@ -222,9 +101,6 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
             red = myCIColor.red
             green = myCIColor.green
             blue = myCIColor.blue
-            redLabel.text = String(red)
-            greenLabel.text = String(green)
-            blueLabel.text = String(blue)
             selectedColorview.backgroundColor = slider.color
             
             //For Finding Actual Colors In For Slider
@@ -243,20 +119,8 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
     // MARK: Methods For Drawing
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
-        // For dismissing keyboard
-        if(textfieldEditing) {
-            self.view.endEditing(true)
-            self.saveStaticVerticalConstraint.constant = 20
-            UIView.animateWithDuration(NSTimeInterval(0.25), animations: {
-                self.containerView.layoutIfNeeded()
-            })
-            textfieldEditing = false
-            if let touch = touches.first {
-                lastPoint = touch.locationInView(self.view)
-            }
-            
-        // For Drawing
-        } else if let touch = touches.first {
+       // For Drawing
+       if let touch = touches.first {
             //Make tap falls within canvas
             let point = touch.locationInView(self.view)
             if(CGRectContainsPoint(self.mainImageView.bounds, point)) {
@@ -272,7 +136,6 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
             
                 swiped = false
             }
- 
         }
     }
     
@@ -296,12 +159,10 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
         tempImageView.alpha = opacity
         UIGraphicsEndImageContext()
         
-        undoButton.hidden = false
-        
         curX = UInt8(Int(floor(fromPoint.x / xratio)))
         curY = UInt8(Int(floor(fromPoint.y / yratio)))
     
-        if ((curX != lastX) && (curY != lastY)) {
+        if ((curX != lastX) || (curY != lastY)) {
             // Write To Buffer
             synced(self, closure: {
                 self.buffer[self.buffEnd] = [self.curX, self.curY, self.colorSlider.colorMapped]
@@ -327,22 +188,20 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if(!textfieldEditing) {
-            if let touch = touches.first {
-                let point = touch.locationInView(self.view)
-                if(CGRectContainsPoint(self.mainImageView.bounds, point)) {
-                    swiped = true
-                    let currentPoint = touch.locationInView(view)
-                    drawLineFrom(lastPoint, toPoint: currentPoint)
         
-                    lastPoint = currentPoint
-                }
+        if let touch = touches.first {
+            let point = touch.locationInView(self.view)
+            if(CGRectContainsPoint(self.mainImageView.bounds, point)) {
+                swiped = true
+                let currentPoint = touch.locationInView(view)
+                drawLineFrom(lastPoint, toPoint: currentPoint)
+                lastPoint = currentPoint
             }
         }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if !swiped && !textfieldEditing {
+        if !swiped {
             // Draw a Single Point
             drawLineFrom(lastPoint, toPoint: lastPoint)
         }
@@ -428,35 +287,6 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
         return globeArray
     }
     
-    // MARK: Helpers for Pixel Color Value Calculations
-    
-    func mask8(value : UInt32) -> UInt32 {
-        return value & UInt32(0xFF)
-    }
-    
-    func redValue(value : UInt32) -> UInt32 {
-        return mask8(value)
-    }
-    
-    func greenValue(value : UInt32) -> UInt32 {
-        return mask8(value >> 8)
-    }
-    
-    func blueValue(value : UInt32) -> UInt32 {
-        return mask8(value >> 16)
-    }
-    
-    func hexValue (color : UIColor) -> UInt32 {
-        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-        if color.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
-            var colorAsUInt : UInt32 = 0
-            colorAsUInt += UInt32(red * 255.0) << 16 + UInt32(green * 255.0) << 8 + UInt32(blue * 255.0)
-            return colorAsUInt
-        } else {
-            return 0
-        }
-    }
-    
     func processBLE(notice:NSNotification) {
         if let userDict = notice.userInfo{
             let resp = userDict["status"] as! Int
@@ -513,16 +343,5 @@ class ArtModeViewController: UIViewController, UITextFieldDelegate {
             }
         }
     }
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
